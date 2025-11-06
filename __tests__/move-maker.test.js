6f7f7012fc9e982a4138a4a180c01238db70d1e3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-06 14:02:55 +0000

    Refactor test formatting for move validation and board update

--- __tests__/move-maker.test.js
+++ __tests__/move-maker.test.js
@@ -1,39 +1,46 @@
-import {makeMove, validateMove} from "../move-maker.js";
+import { makeMove, validateMove } from "../move-maker.js";
 
-test("should return false for an invalid move",()=>{
-    expect(validateMove("1,1",[
-        ['X', '_', '_'],
-        ['_', 'X', '_'],
-        ['O', 'O', 'X']
-    ])).toBe(false)
-    expect(validateMove("0,1",[
-        ['X', '_', '_'],
-        ['_', 'X', '_'],
-        ['O', 'O', 'X']
-    ])).toBe(false)
-})
-test("should return true for a valid move",()=>{
-    expect(validateMove("1,2",[
-        ['X', '_', '_'],
-        ['_', 'X', '_'],
-        ['O', 'O', 'X']
-    ])).toBe(true)
-})
+test("should return false for an invalid move", () => {
+  expect(
+    validateMove("1,1", [
+      ["X", "_", "_"],
+      ["_", "X", "_"],
+      ["O", "O", "X"],
+    ])
+  ).toBe(false);
+  expect(
+    validateMove("0,1", [
+      ["X", "_", "_"],
+      ["_", "X", "_"],
+      ["O", "O", "X"],
+    ])
+  ).toBe(false);
+});
+test("should return true for a valid move", () => {
+  expect(
+    validateMove("1,2", [
+      ["X", "_", "_"],
+      ["_", "X", "_"],
+      ["O", "O", "X"],
+    ])
+  ).toBe(true);
+});
 
 test("should update board for a valid move", () => {
- const board=
- [["X", "_", "_"],
- ["_", "X", "_"],
- ["O", "O", "X"]]
- const move="1,2"
- const player="X"
- const expectedBoard = [
-   ["X", "X", "_"],
-   ["_", "X", "_"],
-   ["O", "O", "X"],
- ];
-   makeMove(board,move,player)
-   console.log(board);
-   
-   expect(board).toEqual(expectedBoard)})
-  
+  const board = [
+    ["X", "_", "_"],
+    ["_", "X", "_"],
+    ["O", "O", "X"],
+  ];
+  const move = "1,2";
+  const player = "X";
+  const expectedBoard = [
+    ["X", "X", "_"],
+    ["_", "X", "_"],
+    ["O", "O", "X"],
+  ];
+  makeMove(board, move, player);
+  console.log(board);
+
+  expect(board).toEqual(expectedBoard);
+});
